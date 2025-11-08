a56cffe9f86302ef8c74db129cf842106cbf0393
Jana Nekvapilová <jana.nekva@gmail.com>
2025-11-08 19:32:06 +0100

    add typing feedback with mistake state and error styling

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -8,16 +8,22 @@ interface IWordboxProp {
 
 const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish }) => {
   const [lettersLeft, setLettersLeft] = useState<string>(word);  
+  const [mistake, setMistake] = useState<boolean>(false);
         
   useEffect(() => {
 
+    console.log(`old mistake: ${mistake}`)
+
     const handleKeyup = (e: KeyboardEvent) => {
       if (e.key === lettersLeft.slice(0,1)) {
+        setMistake(false)
         if (lettersLeft.length === 1) {
           onFinish();
         } 
            setLettersLeft((prev) => prev.slice(1))
-    } 
+    } else {
+      setMistake(true)
+    }
   };
 
     document.addEventListener('keyup', handleKeyup)
@@ -29,7 +35,7 @@ const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish }) => {
   }, [lettersLeft, onFinish])
 
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={mistake ? "wordbox wordbox--mistake" : "wordbox"}>{lettersLeft}</div>
   );
 };
 
