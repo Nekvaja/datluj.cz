53c310abc8ab4e071b78bc02b65c4bf915fadb7b
Jana Nekvapilová <jana.nekva@gmail.com>
2025-11-15 20:03:59 +0100

    Add new levels data file with configuration objects for each game level

---
+++ levels-list.tsx
@@ -0,0 +1,99 @@
+export interface LevelConfig {
+    level: number,
+    wordsTarget: number,
+    time: number,
+}
+
+
+export const levels : LevelConfig[] = [
+    {
+        level: 1,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 2,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 3,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 4,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 5,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 6,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 7,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 8,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 9,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 10,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 11,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 12,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 13,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 14,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 15,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 16,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 17,
+        wordsTarget: 10,
+        time: 30000,
+    },
+    {
+        level: 18,
+        wordsTarget: 10,
+        time: 30000,
+    }
+] 
