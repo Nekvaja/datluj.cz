a9fa7bdadf962e5b61e2d1bce664b0ccc2cb3d69
Jana Nekvapilová <jana.nekva@gmail.com>
2025-11-15 15:15:18 +0100

    Add final Intro texts and apply styling to the Intro component

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -6,12 +6,32 @@ interface IntroProps {
 
 export const Intro = ({onStartGame} : IntroProps) => {
     return (
-        <div className="intro">
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sunt id delectus dicta iure provident qui ullam numquam voluptatem dolor!</div>
-            <button 
-                type="button"
-                onClick={onStartGame}    
-                >Hrát</button>
+      <div className="intro">
+        <img
+          className="intro__image"
+          src="typewriter_black.png"
+          alt="obrázek psacího stroje"
+        />
+        <div className="intro__content-rules">
+          <p>
+            Napiš co nejvíce slov bez chyby v časovém limitu. S každou další
+            úrovní se slova postupně prodlužují a obtížnost roste.
+          </p>
+          <ul>
+            <li>Slovo se započítá pouze tehdy, když je napsané bez chyby.</li>
+            <li>
+              Splň požadovaný počet správně napsaných slov a postoupíš do další
+              úrovně.
+            </li>
+          </ul>
         </div>
-    )
+        <button
+          className="intro__start-button"
+          type="button"
+          onClick={onStartGame}
+        >
+          Start
+        </button>
+      </div>
+    );
 } 
