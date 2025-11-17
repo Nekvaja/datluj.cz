b7410932d57e1524a3a40f314a1018f15bd6c399
Jana Nekvapilová <jana.nekva@gmail.com>
2025-11-17 11:55:31 +0100

    update timing values in data file

--- src/levels-list.tsx
+++ src/levels-list.tsx
@@ -8,92 +8,92 @@ export default interface LevelConfig {
 export const levels : LevelConfig[] = [
     {
         level: 1,
-        wordsTarget: 30000,
-        time: 15000,
+        wordsTarget: 10,
+        time: 20000,
     },
     {
         level: 2,
-        wordsTarget: 7,
-        time: 30000,
+        wordsTarget: 8,
+        time: 20000,
     },
     {
         level: 3,
         wordsTarget: 6,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 4,
         wordsTarget: 5,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 5,
         wordsTarget: 4,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 6,
         wordsTarget: 4,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 7,
         wordsTarget: 3,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 8,
         wordsTarget: 3,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 9,
         wordsTarget: 3,
-        time: 30000,
+        time: 20000,
     },
     {
         level: 10,
         wordsTarget: 3,
-        time: 40000,
+        time: 20000,
     },
     {
         level: 11,
         wordsTarget: 3,
-        time: 40000,
+        time: 20000,
     },
     {
         level: 12,
         wordsTarget: 3,
-        time: 40000,
+        time: 20000,
     },
     {
         level: 13,
         wordsTarget: 3,
-        time: 45000,
+        time: 25000,
     },
     {
         level: 14,
         wordsTarget: 3,
-        time: 45000,
+        time: 25000,
     },
     {
         level: 15,
         wordsTarget: 3,
-        time: 50000,
+        time: 30000,
     },
     {
         level: 16,
         wordsTarget: 3,
-        time: 50000,
+        time: 35000,
     },
     {
         level: 17,
         wordsTarget: 3,
-        time: 60000,
+        time: 40000,
     },
     {
         level: 18,
         wordsTarget: 3,
-        time: 60000,
+        time: 40000,
     }
 ] 
